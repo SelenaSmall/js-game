a66ad2511f704777732821954e4c0e2faacb59c0
SelenaSmall <selenawiththetattoo@gmail.com>
2016-10-24 22:05:41 +1300

    image files

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,17 @@
  * Initialise your modules in this file
  */
 require("../scss/index.scss");  
+'use strict';
+
+var playNow = prompt("Do you want to play?");
+
+switch(playNow) {
+	case 'yes':
+		{
+		var user = prompt("Enter your name");
+		} console.log(user);
+	case 'no': 
+	break;
+	default:
+	break;
+}
